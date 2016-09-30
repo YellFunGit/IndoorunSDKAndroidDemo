52cd1df7a03eb7b8c02a703768b308e9dc6c6d01
sharyuke <363564023@qq.com>
2016-09-30 11:28:07 +0800

    多类型地图加载

--- app/src/main/java/com/indoorun/testnewsdk/ui/MapLoadCustomActivity.java
+++ app/src/main/java/com/indoorun/testnewsdk/ui/MapLoadCustomActivity.java
@@ -2,14 +2,26 @@ package com.indoorun.testnewsdk.ui;
 
 import android.os.Bundle;
 
+import com.indoorun.mapapi.control.Idr;
+import com.indoorun.mapapi.map.gl.IdrMapView;
 import com.indoorun.testnewsdk.R;
 
+import butterknife.BindView;
+
 public class MapLoadCustomActivity extends BaseActionbarActivity {
+
+    @BindView(R.id.map_custom_view)
+    IdrMapView mapView;
+
+    Idr idr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_load_custom);
         setTitleTxt("加载指定楼层");
+        idr = Idr.with(mapView);
+        idr.loadRegion("14428254382730015")// 加载region
+                .loadFloor("14557583851000004");//加载指定楼层
     }
 }
--- app/src/main/java/com/indoorun/testnewsdk/ui/MapLoadSwitcherActivity.java
+++ app/src/main/java/com/indoorun/testnewsdk/ui/MapLoadSwitcherActivity.java
@@ -2,14 +2,30 @@ package com.indoorun.testnewsdk.ui;
 
 import android.os.Bundle;
 
+import com.indoorun.mapapi.control.Idr;
+import com.indoorun.mapapi.map.gl.IdrMapView;
+import com.indoorun.mapapi.view.SpinnerView;
 import com.indoorun.testnewsdk.R;
 
+import butterknife.BindView;
+
 public class MapLoadSwitcherActivity extends BaseActionbarActivity {
+
+    @BindView(R.id.map_switcher_view)
+    IdrMapView mapView;
+
+    @BindView(R.id.map_switcher)
+    SpinnerView spinnerView;
+
+    Idr idr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_load_switcher);
         setTitleTxt("加载楼层切换器");
+        idr = Idr.with(mapView);
+        idr.loadRegion("14428254382730015")// 加载region
+                .loadFloor(spinnerView);//加载楼层切换器
     }
 }
