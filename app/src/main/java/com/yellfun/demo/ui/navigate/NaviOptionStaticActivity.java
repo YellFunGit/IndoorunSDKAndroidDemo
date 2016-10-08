487f906845d17754321ea2988f714ab2c5a3574e
sharyuke <363564023@qq.com>
2016-10-08 15:17:40 +0800

    option static

--- app/src/main/java/com/yellfun/demo/ui/navigate/NaviOptionStaticActivity.java
+++ app/src/main/java/com/yellfun/demo/ui/navigate/NaviOptionStaticActivity.java
@@ -1,14 +1,23 @@
 package com.yellfun.demo.ui.navigate;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 
 import com.indoorun.mapapi.control.Idr;
+import com.indoorun.mapapi.control.NaviOptions;
+import com.indoorun.mapapi.control.map.MapLoader;
+import com.indoorun.mapapi.control.navi.NaviFrom;
+import com.indoorun.mapapi.control.navi.NaviResult;
+import com.indoorun.mapapi.domain.IdrMapRegionFloorUnit;
 import com.indoorun.mapapi.map.gl.IdrMapView;
 import com.indoorun.mapapi.view.SpinnerView;
 import com.yellfun.demo.R;
 import com.yellfun.demo.ui.BaseActionbarActivity;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class NaviOptionStaticActivity extends BaseActionbarActivity {
 
@@ -20,6 +29,24 @@ public class NaviOptionStaticActivity extends BaseActionbarActivity {
 
     Idr idr;
 
+    IdrMapRegionFloorUnit currentUnit;
+
+    @BindView(R.id.navi_layout)
+    View naviLayout;
+
+    /**
+     * 地图加载器，用来添加marker
+     */
+    MapLoader mapLoader;
+
+    @BindView(R.id.navi_unit_name)
+    TextView unitName;
+
+    /**
+     * 导航结果
+     */
+    private NaviResult naviResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +55,16 @@ public class NaviOptionStaticActivity extends BaseActionbarActivity {
         idr = Idr.with(mapView);
         idr.loadRegion("14428254382730015")// 加载region
                 .loadFloor(spinnerView);//加载楼层切换器
+        mapLoader = idr.loadRegion("14428254382730015")// 加载region
+                .onMapUnitClick((mapLoader, unit) -> {
+                    naviLayout.setVisibility(View.VISIBLE);
+                    currentUnit = unit;
+                    unitName.setText(unit.getName());
+                    return true;
+                })
+                .loadFloor(spinnerView);//加载楼层切换器
+        idr.naviOptions().setType(NaviOptions.Type.STATIC_NAVI)//设置成静态导航
+                .setSwitcher(mapLoader);//设置操作marker的对象
     }
 
     @Override
@@ -46,5 +83,39 @@ public class NaviOptionStaticActivity extends BaseActionbarActivity {
     protected void onDestroy() {
         idr.destory();
         super.onDestroy();
+    }
+
+    @OnClick(R.id.navi_start)
+    public void naviStart() {
+        if (currentUnit != null) {
+            mapLoader.removeMarkerByClass(NaviFrom.CLASS_MARKER_START);
+            idr.naviOptions().setFromFloorId(currentUnit.getFloorId())
+                    .setFromPoint(currentUnit.getPointF())
+                    .setFromMarker(R.mipmap.start_position);
+            navi();
+        }
+    }
+
+    @OnClick(R.id.navi_stop)
+    public void naviStop() {
+        if (currentUnit != null) {
+            mapLoader.removeMarkerByClass(NaviFrom.CLASS_MARKER_END);
+            idr.naviOptions().setToFloorId(currentUnit.getFloorId())
+                    .setToPoint(currentUnit.getPointF())
+                    .setToMarker(R.mipmap.car_position);
+            navi();
+        }
+    }
+
+    private void navi() {
+        if (!TextUtils.isEmpty(idr.naviOptions().getFromFloorId())
+                && !TextUtils.isEmpty(idr.naviOptions().getToFloorId())
+                && idr.naviOptions().getFromPoint() != null
+                && idr.naviOptions().getToPoint() != null) {
+            if (naviResult != null) {
+                naviResult.stopNavi();
+            }
+            naviResult = idr.startNavi();
+        }
     }
 }
