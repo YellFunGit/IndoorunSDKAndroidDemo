c33b95c31644277f8edf73a64ab82e09cf34e076
sharyuke <363564023@qq.com>
2017-03-29 18:12:30 +0800

    修改定位服务器为测试服务器

--- app/src/main/java/com/yellfun/demo/App.java
+++ app/src/main/java/com/yellfun/demo/App.java
@@ -11,15 +11,16 @@ import com.indoorun.mapapi.control.Idr;
  */
 public class App extends Application {
 
-    //    public static final String REGION_ID = "14428254382730015";
-    public static final String REGION_ID = "14900020402039555";
-    //    public static final String FLOOR_ID = "14557583851000004";
-    public static final String FLOOR_ID = "14900020402219556";
+        public static final String REGION_ID = "14428254382730015";
+//    public static final String REGION_ID = "14900020402039555";
+        public static final String FLOOR_ID = "14557583851000004";
+//    public static final String FLOOR_ID = "14900020402219556";
 
     @Override
     public void onCreate() {
         super.onCreate();
         Idr.initSDK(this)// 初始化SDK
+                .setLocateUrl("test.locating.indoorun.com")
                 .needCrash();// 拦截崩溃，写入到崩溃日志文件
     }
 }
